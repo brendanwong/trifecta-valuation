52a9d26c212c02cab0863c368a7f6ad2e865e2b2
brendanwong <mrbrendanwong@gmail.com>
2019-09-30 21:48:26 -0700

    idk

--- js/calculations.js
+++ js/calculations.js
@@ -1,5 +1,14 @@
 var bbp = true, gold = true, plat = true, cfu = true, csp = true, csr = true;
+// function displayCalcInfo(){
 
+//     // M.toast({html: 'This is a calculator and visualization comparison of the Chase and American Express Trifectas which utilizes user spend input. If applicable, the annual fees for each card is applied after the first year of calculation. The personal Chase Freedom Unlimited card was chosen in place of the Ink Business Preferred, to compare to the American Express Blue Business Plus for a base level catch-all for non-category spend. With higher spend and/or redemption via transfer partners, it may seem that American Express will net you more return but factors such as benefits valuation/ease of credit use, current transfer partners for either bank, and other confounding variables such as American Express not being as widely accepted should be taken into account.'});
+//      $(document).ready(function(){
+//         $('#test3').modal();
+//       });
+
+
+
+// }
 function setValues(){
     // main driver function, obtains and validates values and sets output in the document
     var monthlyRestaurantSpend, monthlyGrocerySpend, monthlyFlightSpend, monthlyMiscSpend;
