0bba4ae608723dff7549b1d28dd17c2c080c5fb6
brendanwong <mrbrendanwong@gmail.com>
2019-10-03 19:23:39 -0700

    ayylmao

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # AMEX vs Chase Trifecta Valuation Calculator
 A reward points valuation calculator using personal spend for Chase UR and AMEX MR.
+
 check !! it !! out !!!! https://brendanwong.github.io/trifecta-valuation/
+
 TODO:
 * military annual fee waiver
 * decrease vertical size of inline input
 * add sign up bonuses
   * maybe sliders for varying public offers like 50-100k for plat
   * 3x for first year with freedom unlimited
+* make it actually work on mobile
--- js/calculations.js
+++ js/calculations.js
@@ -5,9 +5,10 @@ function setValues(){
     // main driver function, obtains and validates values and sets output in the document
     var monthlyRestaurantSpend, monthlyGrocerySpend, monthlyFlightSpend, monthlyCFSpend, monthlyMiscSpend;
     var monthlySpendTotal, AMEXBenefitsTotal, ChaseBenefitsTotal, monthlyMRTotal, monthlyURTotal;
-    var monthlyMRValuation, yearlyMRValuation, monthlyURValuation;
+    var monthlyMRValuation, yearlyMRValuation, monthlyURValuation, yearlyURValuation;
     var yearlySpendTotal, yearlyMRTotal, yearlyURTotal;
     var ctx;
+    var chaseAF, amexAF;
 
     // monthly personal spend
     monthlyRestaurantSpend = parseFloat(document.getElementById('monthlyRestaurantInput').value);
@@ -39,12 +40,10 @@ function setValues(){
     monthlyURTotal = monthlyURCalculation(monthlyRestaurantSpend, monthlyGrocerySpend, monthlyFlightSpend, monthlyCFSpend, monthlyMiscSpend);
     monthlyURValuation = redemptionMultiplierUR(monthlyURTotal);
 
-    // set values in document
+    // set monthly summaries
     // document.getElementById('monthlyTotal').innerHTML = Math.round(monthlySpendTotal).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-
     // document.getElementById('monthlyTotalMR').innerHTML = Math.round(monthlyMRTotal).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     // document.getElementById('monthlyTotalUR').innerHTML = Math.round(monthlyURTotal).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-
     // document.getElementById('monthlyMRValuation').innerHTML = Math.round(monthlyMRValuation).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     // document.getElementById('monthlyURValuation').innerHTML = Math.round(monthlyURValuation).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
@@ -53,7 +52,7 @@ function setValues(){
     yearlyMRTotal = monthlyMRTotal * 12;
     yearlyURTotal = monthlyURTotal * 12;
     yearlyMRValuation = monthlyMRValuation * 12 + AMEXBenefitsTotal;
-    var yearlyURValuation = monthlyURValuation * 12 + ChaseBenefitsTotal;
+    yearlyURValuation = monthlyURValuation * 12 + ChaseBenefitsTotal;
 
     // set values in document
     document.getElementById('totalOutput').innerHTML = Math.round(yearlySpendTotal).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -66,23 +65,20 @@ function setValues(){
     
     // document.getElementById('yearlyMRValuation').innerHTML = Math.round(yearlyMRValuation).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     // document.getElementById('yearlyURValuation').innerHTML = Math.round(yearlyURValuation).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    var chaseAF = getChaseAF();
-    var amexAF = getAMEXAF();
+    chaseAF = getChaseAF();
+    amexAF = getAMEXAF();
 
 
     document.getElementById('totalAMEXFees').innerHTML = amexAF.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     document.getElementById('totalChaseFees').innerHTML = chaseAF.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-
     monthlyMRValuation = Math.round(monthlyMRValuation);
     monthlyURValuation = Math.round(monthlyURValuation);
 
-    createChart(chaseAF, amexAF, monthlyMRValuation, monthlyURValuation, AMEXBenefitsTotal, ChaseBenefitsTotal);
+    generateChart(chaseAF, amexAF, monthlyMRValuation, monthlyURValuation, AMEXBenefitsTotal, ChaseBenefitsTotal);
 }
 
-function createChart(chaseAF, amexAF, monthlyMRValuation, monthlyURValuation, AMEXBenefitsTotal, ChaseBenefitsTotal){
-    // get annual fees
-  
+function generateChart(chaseAF, amexAF, monthlyMRValuation, monthlyURValuation, AMEXBenefitsTotal, ChaseBenefitsTotal){
     // create function data
     var chaseMonthly = new Array(24);
     var amexMonthly = new Array(24);
@@ -103,6 +99,7 @@ function createChart(chaseAF, amexAF, monthlyMRValuation, monthlyURValuation, AM
         mrVal += monthlyMRValuation;
         months.push(i + 1);
     }
+
     // ok i dont really know whats going on here but extra stuff is appended to the beginning of the arrays
     for (i = 0; i < 24; i++){
         amexMonthly.shift();
@@ -132,42 +129,40 @@ function createChart(chaseAF, amexAF, monthlyMRValuation, monthlyURValuation, AM
                 fill: false
               }
             ]
-          },
-        options: {
-            legend: {fillStyle: Color},
-            responsive: true,
-            title: {
-              display: true,
-              text: 'AMEX MR vs. Chase UR Valuation over 24 months'
-            },
-            tooltips: {
-                mode: 'index',
-                intersect: false,
-            },
-            hover: {
-                mode: 'nearest',
-                intersect: true
-            },
-            scales: {
-                xAxes: [{
-                    display: true,
-                    scaleLabel:{
+              },
+            options: {
+                legend: {fillStyle: Color},
+                responsive: true,
+                title: {
+                  display: true,
+                  text: 'AMEX MR vs. Chase UR Valuation over 24 months'
+                },
+                tooltips: {
+                    mode: 'index',
+                    intersect: false,
+                },
+                hover: {
+                    mode: 'nearest',
+                    intersect: true
+                },
+                scales: {
+                    xAxes: [{
                         display: true,
-                        labelString: 'Month'
-                    }
-                }],
-                yAxes: [{
-                    display: true,
-                    scaleLabel: {
+                        scaleLabel:{
+                            display: true,
+                            labelString: 'Month'
+                        }
+                    }],
+                    yAxes: [{
                         display: true,
-                        labelString: 'Valuation in $'
-                    }
-                }]
-            }
-      }
-
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Valuation in $'
+                        }
+                    }]
+                }
+          }
     });
-
 }
 
 function getChaseAF(){
@@ -176,6 +171,7 @@ function getChaseAF(){
         annualFee += 450;
     return annualFee;
 }
+
 function getAMEXAF(){
     var annualFee = 0;
     if (plat)
@@ -184,6 +180,7 @@ function getAMEXAF(){
         annualFee += 250;
     return annualFee;
 }
+
 function getChaseBenefits(){
     // obtains and validates yearly card benefits
     var travelCredit, miscCredit;
@@ -210,7 +207,6 @@ function getAmexBenefits(){
     saksFifthCredit = parseFloat(document.getElementById('saksFifthInput').value);
     miscCredit = parseFloat(document.getElementById('amexMiscCreditsInput').value);
 
-
     //input validation/check if box is disabled
     if (isNaN(airlineFeeCredit) || document.getElementById('monthlyAirlineFeeInput').disabled)
         airlineFeeCredit = 0;
@@ -225,8 +221,8 @@ function getAmexBenefits(){
     if (isNaN(miscCredit))
         miscCredit = 0;
 
-    // spread out over 4 years 
-    // securityFeeCredit = securityFeeCredit / 4;
+    // spread out over 4 years, temporary add detail later
+    securityFeeCredit = securityFeeCredit / 4;
     return airlineFeeCredit + diningCredit + securityFeeCredit + uberCredit + saksFifthCredit + miscCredit;
 }
 
@@ -289,7 +285,10 @@ function monthlyURCalculation(monthlyRestaurantSpend, monthlyGrocerySpend, month
     } else {
         M.toast({html: 'Select at least one card from Chase for proper valuation.'})  
     }
-    return monthlyRestaurantSpend * restaurantMultiplier + monthlyFlightSpend * travelMultiplier + monthlyCFSpend * freedomMultiplier + monthlyMiscSpend * miscMultiplier
+    return monthlyRestaurantSpend * restaurantMultiplier + 
+        monthlyFlightSpend * travelMultiplier + 
+        monthlyCFSpend * freedomMultiplier + 
+        monthlyMiscSpend * miscMultiplier
 }
 
 function monthlyMRCalculation(monthlyRestaurantSpend, monthlyGrocerySpend, monthlyFlightSpend, monthlyCFSpend, monthlyMiscSpend){
@@ -328,9 +327,10 @@ function monthlyMRCalculation(monthlyRestaurantSpend, monthlyGrocerySpend, month
         monthlyMiscSpend * miscMultiplier;
 }
 
+
 // amex activation functions
 function setPlat(){
-    if (plat == true){
+    if (plat){
         plat = false;
         $("#platToggle").html('<i class="material-icons">add</i>');
         $('#securityFeeCreditInput').prop('disabled', true).addClass('disabled').off( "click" );
@@ -350,8 +350,9 @@ function setPlat(){
         setValues();
     }
 }
+
 function setGold(){
-    if (gold == true){
+    if (gold){
         gold = false;
         $("#goldToggle").html('<i class="material-icons">add</i>'); 
         if (!plat)
@@ -368,8 +369,9 @@ function setGold(){
 
     }
 }
+
 function setBBP(){
-    if (bbp == true){
+    if (bbp){
         bbp = false;
         $("#bbpToggle").html('<i class="material-icons">add</i>'); 
         setValues();
@@ -382,9 +384,11 @@ function setBBP(){
 
     }
 }
+
+
 //chase activation functions
 function setCSR(){
-    if (csr == true){
+    if (csr){
         csr = false;
         $("#csrToggle").html('<i class="material-icons">add</i>'); 
         $('#travelCreditInput').prop('disabled', true).addClass('disabled').off( "click" );
@@ -397,8 +401,9 @@ function setCSR(){
         setValues();
     }
 }
+
 function setCF(){
-    if (cf == true){
+    if (cf){
         cf = false;
         $("#cfToggle").html('<i class="material-icons">add</i>'); 
         setValues();
@@ -409,8 +414,9 @@ function setCF(){
         setValues();
     }
 }
+
 function setCFU(){
-    if (cfu == true){
+    if (cfu){
         cfu = false;
         $("#cfuToggle").html('<i class="material-icons">add</i>'); 
         setValues();
